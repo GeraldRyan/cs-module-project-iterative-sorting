0267d93e04a73d8b2acb052ba9e1f04d98b53eed
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-15 19:32:27 -0500

    got the tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -12,8 +12,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-    if arr[len(arr)/2] > target:
-        return "greater"
+    low = 0
+    mid = round(len(arr)/2)
+    high = len(arr)-1
+
+    while low <= high:
+        mid = round((low + high) / 2)
+
+        if arr[mid] == target:
+            return mid
+        elif target < arr[mid]:
+            high = mid-1
+        else: 
+            low = mid+1
+# arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
 
 
     return -1  # not found
